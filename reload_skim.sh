6f76e0167314ddafb86449a51ee6163d59ef361b
yuuma <yuuma0207@gmail.com>
2025-05-03 15:54:12 +0900

    revised

--- reload_skim.sh
+++ reload_skim.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+ABS_PATH=$(realpath "$1")  # 引数を絶対パスに変換
+
 /usr/bin/osascript << EOF
-  set theFile to POSIX file "$1" as alias
+  set theFile to POSIX file "$ABS_PATH" as alias
   tell application "Skim"
-  activate
-  set theDocs to get documents whose path is (get POSIX path of theFile)
-  if (count of theDocs) > 0 then revert theDocs
-  open theFile
+    activate
+    set theDocs to get documents whose path is (get POSIX path of theFile)
+    if (count of theDocs) > 0 then revert theDocs
+    open theFile
   end tell
 EOF
